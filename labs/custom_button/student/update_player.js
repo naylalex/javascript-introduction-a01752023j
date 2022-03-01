a4f8d8566ae73ede5b7ef7b81a77fce6588b7a8b
a01752023j <69485255+a01752023j@users.noreply.github.com>
2022-03-01 00:54:02 +0000

    Trabajo en clase

--- labs/custom_button/student/update_player.js
+++ labs/custom_button/student/update_player.js
@@ -7,5 +7,12 @@ Presentación de un diálogo por medio de la función prompt()
 Asignación del contenido de texto del párrafo por medio del atributo textContent de la variable para
 para.textContent = 'Some string'
 */
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
 function updateName() {
+  const name = prompt('Enter a new drummer');
+  para.textContent = `Drummer 1: ${name}`;
 }
+
